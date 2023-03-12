0ef298a025ac69ad6307c20b4b6e147edd2cf4a1
Sneha Priyanshu <sneharimipriyanshu@gmail.com>
2023-03-12 23:06:19 +0530

    initial commit

---
+++ src/main/java/com/sneha/musicdatabase/MusicDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.musicdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sneha/musicdatabase/MusicDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.musicdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
